b40a0c8af76e7ff9238d469892260ddbaa37f31a
OZY “OZY” Urbina <URBINA889@gmail.com>
2025-11-10 18:21:06 +0000

    necesitas preparar la app para  log in  y todo lo demas  con  firebase y

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,7 +4,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { getFirestore, Firestore, enableMultiTabIndexedDbPersistence } from 'firebase/firestore';
 
 let firebaseApp: FirebaseApp;
 let auth: Auth;
@@ -27,6 +27,20 @@ if (!getApps().length) {
 
 auth = getAuth(firebaseApp);
 firestore = getFirestore(firebaseApp);
+
+// Enable offline persistence
+enableMultiTabIndexedDbPersistence(firestore).catch((err) => {
+  if (err.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a time.
+    console.warn('Firestore persistence failed: multiple tabs open.');
+  } else if (err.code == 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+     console.warn('Firestore persistence not available in this browser.');
+  }
+});
+
 
 // Now that services are initialized, export them.
 // And also export the function to get them.
